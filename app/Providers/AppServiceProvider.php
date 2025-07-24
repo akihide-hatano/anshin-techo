f29bb4d8a1a8048d852ac3a5b7befc4f4c3a2bad
akihide-hatano <a.h.19841202@gmail.com>
2025-07-24 15:16:46 +0900

    Revert "app/Providers/AppServiceProvider.phpを修正。"
    This reverts commit e5fa98aee10c52d6217b9ad93d6fcc25db68bbf1.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,5 +19,45 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // FCM通知チャネルを登録
+        Notification::extend('fcm', function ($app) {
+            return new class($app->make(Firebase::class)->messaging()) extends Channel {
+                protected $messaging;
+
+                public function __construct(\Kreait\Firebase\Messaging $messaging)
+                {
+                    $this->messaging = $messaging;
+                }
+
+                /**
+                 * Send the given notification.
+                 *
+                 * @param  mixed  $notifiable
+                 * @param  \Illuminate\Notifications\Notification  $notification // この行はコメントアウトまたは削除
+                 * @return void
+                 */
+                // ★★★ ここを修正 ★★★
+                public function send($notifiable, BaseNotification $notification) // BaseNotificationのままにして、toFcmのチェックを追加
+                {
+                    // sendメソッドに渡される$notificationがPushNotificationのインスタンスであることを期待
+                    // toFcmメソッドはPushNotificationクラスに存在するため、型チェックを行う
+                    if (!($notification instanceof PushNotification)) {
+                        Log::warning('Notification is not an instance of PushNotification. Cannot call toFcm method.');
+                        return;
+                    }
+
+                    if (! $message = $notification->toFcm($notifiable)) {
+                        return;
+                    }
+
+                    try {
+                        $this->messaging->send($message);
+                        Log::info('FCM notification sent successfully.');
+                    } catch (\Throwable $e) {
+                        Log::error('FCM notification failed: ' . $e->getMessage());
+                    }
+                }
+            };
+        });
     }
 }
